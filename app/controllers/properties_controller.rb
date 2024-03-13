6a1fef7f837261a7a5596a4d22f142c65228e24e
Yan Meng <yannmm@foxmail.com>
2024-03-13 21:02:37 +0800

    part38 - optimize

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,10 +1,12 @@
 class PropertiesController < ApplicationController
     def show 
-        @property = Property.find(params[:id])
+        @property = Property.includes(:reviews).find(params[:id])
         
         @overall_rating_counts = @property.reviews
         .pluck(:final_rating)
         .group_by { |e| e.round }
         .transform_values { |e| e.length.to_f / @property.reviews_count }
+
+        @overall_rating_counts.default = 0
     end 
 end

97a671ec6d26a88e975d859de3332b94c57eee68
Yan Meng <yannmm@foxmail.com>
2024-03-25 22:04:45 +0800

    part44 - amenities ui

--- app/models/amenity.rb
+++ app/models/amenity.rb
@@ -1,7 +1,7 @@
 class Amenity < ApplicationRecord
     validates :name, presence: true, uniqueness: true
 
-    has_one_attached :icon
+    validates :icon, presence: true
 
     has_many :property_amenities, dependent: :destroy
     

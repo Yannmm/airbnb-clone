b2ecb51ffeb8e0920ee12839c29185440012b5b1
Rayman Yan <yannmm@foxmail.com>
2024-03-26 17:27:28 +0800

    part45 - amenities & reviews modal

---
+++ app/javascript/controllers/modal_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus";
+import { enter, leave, toggle } from "el-transition";
+
+export default class extends Controller {
+  connect() {
+    // this.desktopUserMenuButtonTarget.addEventListener("click", (e) => {
+    //   toggle(this.desktopUserOptionsDropdownTarget);
+    // });
+    // this.mobileUserMenuButtonTarget.addEventListener("click", (e) => {
+    //   toggle(this.mobileUserOptionsDropdownTarget);
+    // });
+  }
+
+  show(event) {
+    const id = event.target.dataset.where;
+    enter(document.getElementById(id));
+  }
+
+  hide(event) {
+    const id = event.target.dataset.where;
+
+    leave(document.getElementById(id));
+  }
+}

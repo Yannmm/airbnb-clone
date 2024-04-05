b83ab683c766903a29e0ba48477c0d11bd79390c
Yan Meng <yannmm@foxmail.com>
2024-04-05 11:19:27 +0800

    part64 - sign up page add parameters

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { 
+    registrations: "users/registrations" 
+  }
+
+  
+
   root 'home#index'
+
   get "up" => "rails/health#show", as: :rails_health_check
+
 
 
   # Equals to below:

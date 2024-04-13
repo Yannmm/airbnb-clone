a256556fb62d0af7b3de5770df8e0e401b8d3861
Yan Meng <yannmm@foxmail.com>
2024-04-13 10:26:06 +0800

    ask user to input current password and password confirmation before changing passwork

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,12 +1,15 @@
 class PasswordsController < ApplicationController 
+    before_action :authenticate_user!
+
     def edit
     end
 
     def update
-        if current_user.update(user_params)
+        if current_user.update_with_password(user_params)
             redirect_to root_path, notice: 'Password updated successfully.'
         else
-            redirect_to root_path, alert: 'Fail to edit Password.'
+            # redirect_to edit_password_path, alert: 'Fail to edit Password.'
+            render :edit, status: :unprocessable_entity
         end
     end
 
@@ -14,6 +17,6 @@ class PasswordsController < ApplicationController
 
     def user_params
         # TODO: how to double check password?
-        params.require(:user).permit(:password)
+        params.require(:user).permit(:password, :password_confirmation, :current_password)
     end
 end

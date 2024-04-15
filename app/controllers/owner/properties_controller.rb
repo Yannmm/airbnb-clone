7413303340f3ca2a1e1a05a4f0dbcf0414a20350
Yan Meng <yannmm@foxmail.com>
2024-04-15 22:15:33 +0800

    part83 - 7:03

--- app/controllers/owner/properties_controller.rb
+++ app/controllers/owner/properties_controller.rb
@@ -3,6 +3,8 @@ module Owner
         before_action :authenticate_user!
 
         before_action :set_user
+
+        before_action :set_property, only: [:edit, :update]
         
         def index 
             @properties = @user.properties
@@ -11,12 +13,21 @@ module Owner
         def new 
         end
 
+        def edit 
+            
+        end
+
+        def update 
+        end
+
         private 
 
         def set_user 
             @user = current_user
         end
 
-
+        def set_property
+            @property = current_user.properties.find(params[:id])
+        end
     end
 end

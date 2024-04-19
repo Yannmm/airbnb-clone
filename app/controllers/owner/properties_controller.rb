cbdcd00f7d5d7bea1c05958640aa6f39b974d789
Yan Meng <yannmm@foxmail.com>
2024-04-19 17:41:32 +0800

    part84 - update amenities done

--- app/controllers/owner/properties_controller.rb
+++ app/controllers/owner/properties_controller.rb
@@ -29,27 +29,31 @@ module Owner
             end
         end
 
+        # def furnish
+
+        #     amenity_ids = amenities_params[:amenity_ids].select { |e| e != "" }.collect { |e| e.to_i }
+
+        #     # ids = @property.amenity_ids.push(amenity_ids).flatten
+        #     # ids.uniq!
+
+        #     @property.amenities.delete_all
+
+        #     amenity_ids.each do |id|
+        #         @property.property_amenities.create!(amenity_id: id)
+        #     end
+
+        #     redirect_to root_path, notice: "Update amenities of property #{@property.id} successfully."
+        # end
+
         def furnish
 
-            amenity_ids = amenities_params[:amenity_ids].select { |e| e != "" }.collect { |e| e.to_i }
-            a1 = @property.amenity_ids
-            
-            a2 = a1.push(amenity_ids).flatten
-
-            # FIXME: why uniq! will make a3 nil?
-            a3 = a2.uniq!
-
-            @property.amenities.delete_all
-
-            a2.each do |id|
-                @property.property_amenities.create(amenity_id: id)
+            if @property.update(amenities_params)
+                redirect_to root_path, notice: "Update amenities of property #{@property.id} successfully."
+            else
+                render :edit, status: unprocessable_entity
             end
 
-
-            
-
-
-            redirect_to root_path, notice: "Update amenities of property #{@property.id} successfully."
+   
         end
 
         private 
